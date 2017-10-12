495ab9930cb4ff59c0ae04f5a536a0f7e2b9111a
Ryan Colley <u1356831@hud.ac.uk>
2017-10-12 20:06:35 +0100

    Third Commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel Test Ryan
+                    Laravel Test Ryan Colley
                 </div>
 
                 <div class="links">
